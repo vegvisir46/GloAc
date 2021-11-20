cd36d7c2dbe347ac16741529a70910c3ac1dd173
vegvisir <alexandr.rostow@gmail.com>
2021-11-20 21:04:01 +0700

    second commit

--- js/getGoods.js
+++ js/getGoods.js
@@ -2,12 +2,32 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
+    const links = document.querySelectorAll('.navigation-link');
 
-    fetch('https://gloac-7e64b-default-rtdb.europe-west1.firebasedatabase.app/db.json')
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-        })
+    const getData = () => {
+        fetch('https://gloac-7e64b-default-rtdb.europe-west1.firebasedatabase.app/db.json')
+            .then((res) => res.json())
+            .then((data) => {
+                localStorage.setItem('goods', JSON.stringify(data));
+            });
+    }
 
+    links.forEach(item => {
+        item.addEventListener('click', (event) => {
+            event.preventDefault();
+            getData();
+        });
+    });
+
+    getData();
+
+    // console.log(JSON.parse(localStorage.goods));
+
+    // localStorage.setItem('goods', JSON.stringify({name: 'all'}));
+    // localStorage.setItem('goods', JSON.stringify({name: 'all'}));
+    // console.log(JSON.parse(localStorage.getItem('goods')));
+    // const goods = JSON.parse(localStorage.getItem('goods'));
+    // console.log(goods);
+    // localStorage.removeItem('goods');
 
 });
