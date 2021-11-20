9523aba1b8d1144341c14abe7480021898b6f03c
vegvisir <alexandr.rostow@gmail.com>
2021-11-20 20:12:42 +0700

    second commit

--- js/getGoods.js
+++ js/getGoods.js
@@ -2,7 +2,11 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
-    // fetch('/db/db.json');
+    fetch('https://gloac-7e64b-default-rtdb.europe-west1.firebasedatabase.app/db.json')
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+        })
 
 
 
